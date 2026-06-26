751e202ea577c5b4df5009ed3e633d4f1a3dcd0f
aspera24 <serbeatmaker@gmail.com>
2026-06-26 15:59:03 +0800

    Update app js

--- public/app.js
+++ public/app.js
@@ -126,7 +126,7 @@ async function ensureMediaReady(attempt = 0) {
             audio: {
                 echoCancellation: false,
                 noiseSuppression: true,
-                autoGainControl: false,
+                autoGainControl: true,
                 suppressLocalAudioPlayback: true,
 
                 sampleRate: 48000,
