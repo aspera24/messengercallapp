7607d8d1a65ab27e98db8d6374f1cf14633b0ad0
aspera24 <serbeatmaker@gmail.com>
2026-07-04 13:58:36 +0800

    update server

--- server.js
+++ server.js
@@ -23,8 +23,7 @@ app.use(session({
     saveUninitialized: false,
     cookie: {
         maxAge: 24 * 60 * 60 * 1000,
-        sameSite: "none",   // IMPORTANT
-        secure: true        // IMPORTANT (needs HTTPS)
+      
     }
 }));
 
